a3a838c81430991bbf9b670bbbcd47554034882a
Firas <97522852+fifolio@users.noreply.github.com>
2026-08-11 15:08:39 +0000

    py-9

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Python Tutorial 2026 | Complete Python Course (Beginner to Advanced) | Telusko
 ---
 
 ✅ Python Setup | 3 
+✅ Python Tuple | 9
---
+++ py-9.py
@@ -0,0 +1,12 @@
+list= [45, 87, 21, 24, 99]
+print(list)
+print(type(list))
+tup = (45, 87, 21, 24, 99)
+print(tup)
+print(type(tup))
+
+# -------------------------------
+
+tupB = (34, 'fira', [11, 22, 33])
+print(tupB)
+print(type(tupB))
